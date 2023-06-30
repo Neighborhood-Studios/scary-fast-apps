e8b90416d76dfd27eeba9c2e39099b0436714d67
Igor Molyarov <igor.molyarov@intelligichain.com>
2023-06-30 14:34:52 +0200

    [FE] update logout redirect

--- frontend/src/Services/auth/LoginButton.tsx
+++ frontend/src/Services/auth/LoginButton.tsx
@@ -1,7 +1,7 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import { Link } from 'react-router-dom';
 //
-import { ROUTES } from 'routes.tsx';
+import { getAbsoluteRouteURL, ROUTES } from 'routes.tsx';
 
 export const LoginButton = () => {
     const { loginWithRedirect, logout, isAuthenticated, isLoading } =
@@ -14,7 +14,17 @@ export const LoginButton = () => {
                 <Link to={ROUTES.PROTECTED + '/profile'}>user profile</Link>
             </div>
             {isAuthenticated ? (
-                <button onClick={() => logout()}>Log out</button>
+                <button
+                    onClick={() =>
+                        logout({
+                            logoutParams: {
+                                returnTo: getAbsoluteRouteURL(ROUTES.ROOT),
+                            },
+                        })
+                    }
+                >
+                    Log out
+                </button>
             ) : (
                 <button onClick={() => loginWithRedirect()}>Log In</button>
             )}
--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -8,7 +8,8 @@ import { UserProfile } from 'Pages/UserProfile';
 
 export const baseURL = new URL(import.meta.env.BASE_URL, window.origin);
 export const getAbsoluteRouteURL = (route: string) =>
-    new URL(route.replace(/^\//, ''), baseURL.href.replace(/([^/])$/, '$1/'));
+    new URL(route.replace(/^\//, ''), baseURL.href.replace(/([^/])$/, '$1/'))
+        .href;
 
 export enum ROUTES {
     ROOT = '/',
