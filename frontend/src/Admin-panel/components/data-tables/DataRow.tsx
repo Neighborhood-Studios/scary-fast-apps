2e7b1971ceeb810471c9e2c48889e4f2d74230db
Igor Molyarov <igor.molyarov@intelligichain.com>
2023-07-16 15:22:22 +0200

    [FE] admin-panel: update table data rows

---
+++ frontend/src/Admin-panel/components/data-tables/DataRow.tsx
@@ -0,0 +1,99 @@
+import type { FC } from 'react';
+import { useState } from 'react';
+import { Link, useOutletContext } from 'react-router-dom';
+
+import type { OutletContextType } from '../../pages/DataTables/DataTables.tsx';
+import { DeleteItem } from './DeleteItem.tsx';
+import {
+    getFieldsForModel,
+    pkDelimiter,
+} from '../../pages/DataTables/utils.ts';
+import { ReactComponent as EditSVG } from '../../images/actions/edit.svg';
+
+type DataRowProps = {
+    tableName: string;
+    rowData: Record<string, object>;
+    columns: string[];
+    primaryKeys: string[];
+    canEdit?: boolean;
+    canDelete?: boolean;
+    refetch(): void;
+};
+export const DataRow: FC<DataRowProps> = ({
+    tableName,
+    rowData,
+    columns,
+    primaryKeys,
+    canEdit,
+    canDelete,
+    refetch,
+}) => {
+    const schemaData = useOutletContext<OutletContextType>();
+    const modelFields = getFieldsForModel(schemaData, tableName || '');
+
+    const [isDeleting, setIsDeleting] = useState(false);
+
+    return (
+        <tr className={isDeleting ? 'opacity-50' : ''}>
+            {canEdit || canDelete ? (
+                <td className="border-b border-[#eee] py-5 px-4 dark:border-strokedark">
+                    <div className="flex flex-row flex-nowrap gap-2 align-middle">
+                        {canDelete && (
+                            <DeleteItem
+                                name={tableName}
+                                data={rowData}
+                                onDeleteError={() => {
+                                    setIsDeleting(false);
+                                }}
+                                onDeleteSuccess={refetch}
+                                onDeleteStart={() => setIsDeleting(true)}
+                                disabled={isDeleting}
+                            />
+                        )}
+                        {canEdit && (
+                            <Link
+                                title="edit row"
+                                className="leading-[0px]"
+                                to={primaryKeys
+                                    .map((pk) => rowData[pk])
+                                    .join(pkDelimiter)}
+                            >
+                                <button className="hover:text-primary">
+                                    <EditSVG />
+                                </button>
+                            </Link>
+                        )}
+                    </div>
+                </td>
+            ) : null}
+            {columns.map((colName) => (
+                <td
+                    className="border-b border-[#eee] py-5 px-4 dark:border-strokedark"
+                    key={colName}
+                >
+                    <p className="text-black dark:text-white">
+                        {renderColumnValue(
+                            modelFields[colName],
+                            rowData[colName]
+                        )}
+                    </p>
+                </td>
+            ))}
+        </tr>
+    );
+};
+
+function renderColumnValue(
+    colData: ReturnType<typeof getFieldsForModel>[string],
+    value: string | number | boolean | unknown
+) {
+    switch (colData.type) {
+        case 'timestamptz':
+            return new Date(String(value)).toLocaleString();
+        case 'Boolean':
+        case 'Int':
+        case 'String':
+        default:
+            return String(value);
+    }
+}
---
+++ frontend/src/Admin-panel/components/data-tables/DeleteItem.tsx
@@ -0,0 +1,64 @@
+import type { FC } from 'react';
+import { useMemo } from 'react';
+import { gql, useMutation } from '@apollo/client';
+import { useOutletContext } from 'react-router-dom';
+
+import { OutletContextType } from '../../pages/DataTables/DataTables.tsx';
+import { generateDeleteMutation } from '../../pages/DataTables/utils.ts';
+
+import { ReactComponent as DeleteSVG } from '../../images/actions/delete.svg';
+
+type DeleteItemProps = {
+    name: string;
+    data: Record<string, object>;
+    disabled: boolean;
+    onDeleteStart(): void;
+    onDeleteSuccess(): void;
+    onDeleteError(): void;
+};
+export const DeleteItem: FC<DeleteItemProps> = ({
+    name,
+    data,
+    disabled,
+    onDeleteStart,
+    onDeleteSuccess,
+    onDeleteError,
+}) => {
+    const schemaData = useOutletContext<OutletContextType>();
+    const {
+        mutationString: mutationDeleteString,
+        // mutationName,
+        args: deleteArgs,
+    } = generateDeleteMutation(schemaData, name);
+    const deleteMutation = useMemo(
+        () => gql(mutationDeleteString),
+        [mutationDeleteString]
+    );
+    const [deleteItem] = useMutation(deleteMutation);
+
+    const onDeleteItem = () => {
+        const confirmResult = confirm('delete item?');
+        if (confirmResult) {
+            const variables = deleteArgs.reduce(
+                (variables, { name, varName }) =>
+                    Object.assign(variables, { [varName]: data[name] }),
+                {}
+            );
+            onDeleteStart();
+            deleteItem({ variables })
+                .then(onDeleteSuccess)
+                .catch(onDeleteError);
+        }
+    };
+
+    return (
+        <button
+            title="delete row"
+            className="hover:text-primary"
+            onClick={onDeleteItem}
+            disabled={disabled}
+        >
+            <DeleteSVG />
+        </button>
+    );
+};
