f3d65d1a695fd2e3096e1d37ae28e1fe6764e155
Nikolay Kurevin <nikolay.kurevin@intelligichain.com>
2023-07-21 14:10:00 +0200

    A bit of DB timestamp trigger automatization

--- backend/django/core_utils/models.py
+++ backend/django/core_utils/models.py
@@ -1,8 +1,9 @@
 from django.db import models
 
+
 class BaseModel(models.Model):
-    created_date = models.DateTimeField(auto_now_add=True, db_index=True)
-    updated_date = models.DateTimeField(auto_now=True, db_index=True)
+    created_at = models.DateTimeField(auto_now_add=True, db_index=True)
+    updated_at = models.DateTimeField(auto_now=True, db_index=True)
 
     class Meta:
         abstract = True
